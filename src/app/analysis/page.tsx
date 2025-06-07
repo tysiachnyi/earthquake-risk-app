63ba63b8a1a83bfeec527fdabc691e254cc820b4
Nik <nikita.tysiachnyi@gmail.com>
2025-06-07 11:14:07 +0200

    fxi errors

--- src/app/analysis/page.tsx
+++ src/app/analysis/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import dynamic from "next/dynamic";
-import { Header } from "../components";
 import { format, subMonths, subYears } from "date-fns";
 import {
   LineChart,
